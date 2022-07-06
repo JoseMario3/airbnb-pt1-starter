7d06e84342b166801b83f001d7c65aaec86bfce7
JoseMario3 <f.jose@wustl.edu>
2022-07-06 17:40:40 -0500

    Week 5, Lab 2, End of Day

--- kavholm-express-api/models/booking.test.js
+++ kavholm-express-api/models/booking.test.js
@@ -1,89 +1,140 @@
-const { BadRequestError } = require("../utils/errors")
-const Listing = require("./listing")
-const Booking = require("./booking")
+const { BadRequestError } = require("../utils/errors");
+const Listing = require("./listing");
+const Booking = require("./booking");
 const {
-  commonBeforeAll,
-  commonBeforeEach,
-  commonAfterEach,
-  commonAfterAll,
-  testListingIds,
-} = require("../tests/common")
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll,
+    testListingIds,
+} = require("../tests/common");
 
-beforeAll(commonBeforeAll)
-beforeEach(commonBeforeEach)
-afterEach(commonAfterEach)
-afterAll(commonAfterAll)
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
 
 describe("Booking", () => {
-  describe("Test listBookingsFromUser", () => {
-    test("Fetches all of the authenticated users' bookings", async () => {
-      const user = { username: "jlo" }
-      const listingId = testListingIds[0]
-      const listing = await Listing.fetchListingById(listingId)
 
-      const bookings = await Booking.listBookingsFromUser(user)
-      expect(bookings.length).toEqual(2)
+    describe("Test createBooking", () => {
+        test("Can create a new booking with valid params", async() => {
+            const user = { username: "jlo" };
+            const listingId = testListingIds[0];
+            const listing = await Listing.fetchListingById(listingId);
 
-      const firstBooking = bookings[bookings.length - 1]
+            const newBooking = {
+                startDate : new Date("03-05-2021"),
+                endDate : new Date("03-07-2021"),
+                guests : 20
+            }
 
-      firstBooking.totalCost = Number(firstBooking.totalCost)
+            const booking = await Booking.createBooking({user, listing, newBooking});
 
-      expect(firstBooking).toEqual({
-        id: expect.any(Number),
-        startDate: new Date("03-05-2021"),
-        endDate: new Date("03-07-2021"),
-        paymentMethod: "card",
-        guests: 1,
-        username: "jlo",
-        hostUsername: "lebron",
-        totalCost: Math.ceil(3 * (Number(listing.price) + Number(listing.price) * 0.1)),
-        listingId: listingId,
-        userId: expect.any(Number),
-        createdAt: expect.any(Date),
-      })
+            expect(booking).toEqual({
+                id: expect.any(Number),
+                startDate: newBooking.startDate,
+                endDate: newBooking.endDate,
+                paymentMethod: "card",
+                listingId: listingId,
+                guests: newBooking.guests,
+                username: user.username,
+                userId: expect.any(Number),
+                createdAt: expect.any(Date),
+            });
+        })
+    
+        test("Throws error with invalid params", async() => {
+
+            const user = { username: "jlo" };
+            const listingId = testListingIds[0];
+            const listing = await Listing.fetchListingById(listingId);
+
+            const newBooking = {
+                endDate : new Date("03-07-2021"),
+            }
+
+            try {
+                await Booking.createBooking({user, listing, newBooking});
+            } catch (err) {
+                expect(err instanceof BadRequestError).toBeTruthy();
+            }
+
+        })
     })
 
-    test("Returns empty array when user hasn't booked anything", async () => {
-      const user = { username: "lebron" }
+    describe("Test listBookingsFromUser", () => {
+        test("Fetches all of the authenticated users' bookings", async() => {
+            const user = { username: "jlo" };
+            const listingId = testListingIds[0];
+            const listing = await Listing.fetchListingById(listingId);
 
-      const bookings = await Booking.listBookingsFromUser(user)
-      expect(bookings).toHaveLength(0)
-    })
-  })
+            const bookings = await Booking.listBookingsFromUser(user);
+            expect(bookings.length).toEqual(2);
 
-  describe("Test listBookingsForUserListings", () => {
-    test("Fetches all of the bookings for any listing the user owns", async () => {
-      const user = { username: "lebron" }
-      const listingId = testListingIds[0]
-      const listing = await Listing.fetchListingById(listingId)
+            const firstBooking = bookings[bookings.length - 1];
 
-      const bookings = await Booking.listBookingsForUserListings(user)
-      expect(bookings.length).toEqual(2)
+            firstBooking.totalCost = Number(firstBooking.totalCost);
 
-      const firstBooking = bookings[bookings.length - 1]
+            expect(firstBooking).toEqual({
+                id: expect.any(Number),
+                startDate: new Date("03-05-2021"),
+                endDate: new Date("03-07-2021"),
+                paymentMethod: "card",
+                guests: 1,
+                username: "jlo",
+                hostUsername: "lebron",
+                totalCost: Math.ceil(
+                    3 * (Number(listing.price) + Number(listing.price) * 0.1)
+                ),
+                listingId: listingId,
+                userId: expect.any(Number),
+                createdAt: expect.any(Date),
+            });
+        });
 
-      firstBooking.totalCost = Number(firstBooking.totalCost)
+        test("Returns empty array when user hasn't booked anything", async() => {
+            const user = { username: "lebron" };
 
-      expect(firstBooking).toEqual({
-        id: expect.any(Number),
-        startDate: new Date("03-05-2021"),
-        endDate: new Date("03-07-2021"),
-        paymentMethod: "card",
-        guests: 1,
-        username: "jlo",
-        hostUsername: "lebron",
-        totalCost: Math.ceil(3 * (Number(listing.price) + Number(listing.price) * 0.1)),
-        listingId: listingId,
-        userId: expect.any(Number),
-        createdAt: expect.any(Date),
-      })
-    })
+            const bookings = await Booking.listBookingsFromUser(user);
+            expect(bookings).toHaveLength(0);
+        });
+    });
 
-    test("Returns empty array when users listing have no bookings", async () => {
-      const user = { username: "serena" }
+    describe("Test listBookingsForUserListings", () => {
+        test("Fetches all of the bookings for any listing the user owns", async() => {
+            const user = { username: "lebron" };
+            const listingId = testListingIds[0];
+            const listing = await Listing.fetchListingById(listingId);
 
-      const bookings = await Booking.listBookingsForUserListings(user)
-      expect(bookings).toHaveLength(0)
-    })
-  })
-})
+            const bookings = await Booking.listBookingsForUserListings(user);
+            expect(bookings.length).toEqual(2);
+
+            const firstBooking = bookings[bookings.length - 1];
+
+            firstBooking.totalCost = Number(firstBooking.totalCost);
+
+            expect(firstBooking).toEqual({
+                id: expect.any(Number),
+                startDate: new Date("03-05-2021"),
+                endDate: new Date("03-07-2021"),
+                paymentMethod: "card",
+                guests: 1,
+                username: "jlo",
+                hostUsername: "lebron",
+                totalCost: Math.ceil(
+                    3 * (Number(listing.price) + Number(listing.price) * 0.1)
+                ),
+                listingId: listingId,
+                userId: expect.any(Number),
+                createdAt: expect.any(Date),
+            });
+        });
+
+        test("Returns empty array when users listing have no bookings", async() => {
+            const user = { username: "serena" };
+
+            const bookings = await Booking.listBookingsForUserListings(user);
+            expect(bookings).toHaveLength(0);
+        });
+    });
+});
